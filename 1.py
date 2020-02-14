de07f8e30913ce52ce5c2503e5afc316d0f79055
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2020-02-14 16:29:09 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -5,11 +5,10 @@ from PIL import Image
 from io import BytesIO
 from toponym_envelope import get_toponym_envelope
 
+
 def get_toponym():
     screen.fill((0, 0, 0))
-    intro_text = [
-        "Координаты: ",
-    ]
+    intro_text = ["Координаты: "]
     title = pygame.font.Font(None, 120).render("Жду.", 1, pygame.Color("white"))
     screen.blit(title, ((width - title.get_rect().width) // 2, 200))
     font = pygame.font.Font(None, 30)
@@ -42,9 +41,16 @@ def get_toponym():
                 )
                 pygame.draw.rect(screen, (0, 0, 0), (0, 400, 10000, 400))
                 screen.blit(title, ((width - title.get_rect().width) // 2, 400))
-
         pygame.display.flip()
 
+
+def button(q):
+    pygame.draw.rect(screen, (0, 0, 255), (0, 0, 600, 50))
+    data = ["map", "sat", "sat,skl", "sat,trf", "map,trf"]
+    q %= len(data)
+    text = pygame.font.Font(None, 24).render("Карта", 1, (255, 255, 0))
+    screen.blit(text, (290, 15))
+    return data[q]
 
 
 def mas_minus(spn):
@@ -59,8 +65,8 @@ def mas_plus(spn):
     return spn
 
 
-def get_image(ll, spn, finded_place):
-    map_params = {"ll": ll, "spn": spn, "l": "map", 'pt': finded_place}
+def get_image(ll, spn, mapp, finded_place):
+    map_params = {"ll": ll, "spn": spn, "l": mapp, "pt": finded_place}
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
 
@@ -70,25 +76,31 @@ def get_image(ll, spn, finded_place):
     screen.blit(pygame.image.load("1.png"), (0, 50))
     pygame.display.flip()
 
+
 def up(ll):
     ll = ll.split(",")
     ll = ",".join([str(float(ll[0])), str(float(ll[1]) * 1.00001)])
     return ll
+
 
 def down(ll):
     ll = ll.split(",")
     ll = ",".join([str(float(ll[0])), str(float(ll[1]) / 1.00001)])
     return ll
 
+
 def left(ll):
     ll = ll.split(",")
     ll = ",".join([str(float(ll[0]) / 1.00001), str(float(ll[1]))])
     return ll
 
+
 def right(ll):
     ll = ll.split(",")
     ll = ",".join([str(float(ll[0]) * 1.00001), str(float(ll[1]))])
     return ll
+
+
 size = width, height = 600, 500
 screen = pygame.display.set_mode(size)
 pygame.init()
@@ -112,31 +124,39 @@ toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
 ]
 
 ll, spn = get_toponym_envelope(toponym)
-finded_place = ll + ',' + 'pmgnm'
-get_image(ll, spn, finded_place)
+finded_place = ll + "," + "pmgnm"
+q = 0
+mapp = button(q)
+get_image(ll, spn, mapp, finded_place)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if y < 50:
+                q += 1
+                mapp = button(q)
+                get_image(ll, spn, mapp, finded_place)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
                 spn = mas_minus(spn)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp, finded_place)
             if event.key == pygame.K_PAGEDOWN:
                 spn = mas_plus(spn)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp, finded_place)
             if event.key == pygame.K_UP:
                 ll = up(ll)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp, finded_place)
             if event.key == pygame.K_DOWN:
                 ll = down(ll)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp, finded_place)
             if event.key == pygame.K_RIGHT:
                 ll = right(ll)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp, finded_place)
             if event.key == pygame.K_LEFT:
                 ll = left(ll)
-                get_image(ll, spn, finded_place)
+                get_image(ll, spn, mapp)
     pygame.display.flip()
 pygame.quit()
