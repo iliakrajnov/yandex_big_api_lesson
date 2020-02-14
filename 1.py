7f171a9065245835d11c8d1cbb1048fc4321162a
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2020-02-14 15:02:08 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -6,9 +6,32 @@ from io import BytesIO
 from toponym_envelope import get_toponym_envelope
 
 
+def mas_minus(spn):
+    spn = spn.split(",")
+    spn = ",".join([str(float(spn[0]) * 2), str(float(spn[1]) * 2)])
+    return spn
+
+
+def mas_plus(spn):
+    spn = spn.split(",")
+    spn = ",".join([str(float(spn[0]) / 2), str(float(spn[1]) / 2)])
+    return spn
+
+
+def get_image(ll, spn):
+    map_params = {"ll": ll, "spn": spn, "l": "map"}
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    file = open("1.png", "wb")
+    file.write(response.content)
+    file.close()
+    screen.blit(pygame.image.load("1.png"), (0, 50))
+    pygame.display.flip()
+
+
 size = width, height = 600, 500
 screen = pygame.display.set_mode(size)
-
 
 toponym_to_find = "Москва, ул. Академика Королева 12"
 
@@ -29,27 +52,20 @@ toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
     "GeoObject"
 ]
 
-
-ll, spn = get_toponym_envelope(toponym)
-map_params = {"ll": ll, "spn": spn, "l": "map"}
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
 pygame.init()
-
-file = open("1.png", "wb")
-file.write(response.content)
-file.close()
-while True:
-    screen.blit(pygame.image.load("1.png"), (0, 50))
+ll, spn = get_toponym_envelope(toponym)
+get_image(ll, spn)
+while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
-                print('Pageup')
+                spn = mas_minus(spn)
+                get_image(ll, spn)
             if event.key == pygame.K_PAGEDOWN:
-                print('Pagedown')
-        pygame.display.flip()
-
+                spn = mas_plus(spn)
+                get_image(ll, spn)
+    pygame.display.flip()
 pygame.quit()
